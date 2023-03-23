6da042837192413bc6342bce9ca94896c2d7434e
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-23 15:28:06 -0500

    finishing adding page elements

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,7 +2,13 @@
 
 // Declaring Elements
 
-
+const timer = document.querySelector('.timer');
+const currentWord = document.querySelector('.curr-word');
+const textInp = document.querySelector('#txt-input');
+const reStartBtn = document.querySelector('#start-reset');
+const currentScore = document.querySelector('.curr-score');
+const highScore = document.querySelector('.high-score');
+const higherScore = document.querySelector('.higher-score');
 
 // Class Definition
 
